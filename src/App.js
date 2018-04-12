ef7e58027b8fdf101e090d1b5e85ebfe18c39026
Cameron Rabunn <raburncameron@gmail.com>
2018-04-11 18:05:21 -0600

    all three components are called to

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Home from './Home'
 import JobHistory from './JobHistory'
+import Projects from './Projects'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Home/>
+      {/* <Home/> */}
       {/* <JobHistory /> */}
+      <Projects />
       </div>
     );
   }
