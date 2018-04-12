9a4bce6ce6b8cca5b023c91c1eac863411d1b019
Cameron Rabunn <raburncameron@gmail.com>
2018-04-11 20:48:57 -0600

    added router, link and switch to better link to different pages

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import Home from './Home'
 import JobHistory from './JobHistory'
 import Projects from './Projects'
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      {/* <Home/> */}
-      {/* <JobHistory /> */}
-      <Projects />
-      </div>
+      <Router>
+        <div className="App">
+        <nav>
+          <Link className="navLinks" to="/">Home</Link>
+          <Link className="navLinks" to="/jobhistory">Job History</Link>
+          <Link className="navLinks" to="/projects">Projects</Link>
+        </nav>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/JobHistory" component={JobHistory} />
+            <Route path="/Projects" component={Projects} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
