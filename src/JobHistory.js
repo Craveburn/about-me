2db3b5ffb41a946a7986f6fa617e572560db6124
Cameron Rabunn <raburncameron@gmail.com>
2018-04-11 20:49:36 -0600

    updated to better fit Job.js

--- src/JobHistory.js
+++ src/JobHistory.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ListJobs from './ListJobs'
+import Job from './Job'
 import './JobHistory.css'
 
 class JobHistory extends Component {
@@ -8,13 +8,13 @@ class JobHistory extends Component {
             <div className='main'>
             <div className='jobHistory'>Job History</div>
                 <div className='job1'>
-                    <ListJobs jobName='Best Buy Team Lead' time='3 Years' desc='I am in charge of 8 employees' />
+                    <Job jobName='Best Buy Team Lead' time='3 Years' desc='I am in charge of 8 employees' />
                 </div>
                 <div className='job2'>
-                    <ListJobs jobName='Big 5 Cashier' time='.5 Years' desc='I was a cashier/iventory specialist' />
+                    <Job jobName='Big 5 Cashier' time='.5 Years' desc='I was a cashier/iventory specialist' />
                 </div>
                 <div className='job3'>
-                    <ListJobs jobName='Missionary' time='2 Years' desc='Evangelical Ministiring to people in Everett, Washington' />
+                    <Job jobName='Missionary' time='2 Years' desc='Evangelical Ministiring to people in Everett, Washington' />
                 </div>
             </div>
         );
