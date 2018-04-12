f11ff67fdfb98d5014f23641931128e8d5e1ac71
Cameron Rabunn <raburncameron@gmail.com>
2018-04-11 18:07:13 -0600

    added another prop for projects component

--- src/ListJobs.js
+++ src/ListJobs.js
@@ -5,9 +5,10 @@ class ListJobs extends Component {
     render() {
       return (
         <div>
-        <div className='jobName'>  {this.props.jobName} </div>
+        <div className='jobName'> {this.props.jobName} </div>
         <div className='time'> {this.props.time} </div>
         <div className='desc'> {this.props.desc} </div>
+        <div className='tech'> {this.props.tech} </div>
         </div>
       );
     }
