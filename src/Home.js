a7c9bdfff1986c96e439eb122b2fed377d00b8e2
Cameron Rabunn <raburncameron@gmail.com>
2018-04-16 18:35:21 -0600

    added social media icons and fixed some css issues.

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Home.css'
+import { SocialIcon } from 'react-social-icons'
 
 class Home extends Component {
     render() {
@@ -11,33 +12,47 @@ class Home extends Component {
                     </div>
                     <div className='myName'>
                         Cameron Raburn
+                     </div>
                 </div>
+                <div className="social">
+                    <div className="linkedIn">
+                        <SocialIcon url="https://www.linkedin.com/in/cameronraburn/" color="black" />
+                    </div>
+                    <div className="github"> <SocialIcon url="https://github.com/Craveburn " color="black" />
+                    </div>
+                    <div className="twitter">
+                        <SocialIcon url="https://twitter.com/CRaveBurn" color="black" />
+                    </div>
                 </div>
                 <div className='bio'>
                     <div className='bioTitle'>
                         Bio
                     </div>
                     <div className='bioText'>
-                        Cameron Raburn was born in Provo, Utah August 30th, 1994 to Rebecca and James Raburn. 
+                        Cameron Raburn was born in Provo, Utah August 30th, 1994 to Rebecca and James Raburn.
+                        He has lived in every major city in the State of Utah. He went to Westlake High School and graduated
+                        in 2012 at the age of 17. In 2013 he served a two year service mission for the Church of Jesus Christ of Latter
+                        Day Saints. Upon returning he met his wife Taylor in 2015 and married her March 21st, 2016.
+                        Together they created a child named Everett, and now they live happily in Utah County.
                     </div>
-                </div>
-                <div className='techInt'>
-                    <div className='techTitle'>
-                        Tech Interests/Skills
+                    <div className='techInt'>
+                        <div className='techTitle'>
+                            Tech Interests/Skills
                     </div>
-                    <div className='techCont'>
-                        <ul>
-                            <li>HTML/CSS</li>
-                            <li>Git</li>
-                            <li>JavaScript/JSON</li>
-                            <li>React</li>
-                            <li>NodeJS</li>
-                            <li>Express</li>
-                            <li>RESTful APIs</li>
-                            <li>MongoDB</li>
-                            <li>GitHub</li>
-                            <li>GraphQL</li>
-                        </ul>
+                        <div className='techCont'>
+                            <ul>
+                                <li>HTML/CSS</li>
+                                <li>Git</li>
+                                <li>JavaScript/JSON</li>
+                                <li>React</li>
+                                <li>NodeJS</li>
+                                <li>Express</li>
+                                <li>RESTful APIs</li>
+                                <li>MongoDB</li>
+                                <li>GitHub</li>
+                                <li>GraphQL</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
